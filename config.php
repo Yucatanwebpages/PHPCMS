71aa2c0c69ec4370b7cb2bcd30766d7a96d28cde
Yucatanwebpages <office@yucatanwebpages.com>
2020-11-05 14:35:03 -0500

    First commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'cms');
+define('DB_USER', 'cms_www');
+define('DB_PASS', '');
+
+define('SMTP_HOST', '');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
+define('SMTP_PORT', '587');
+define('SMTP_SECURE', 'tls');
+
+define ('SHOW_ERROR_DETAIL', true); // set it to "true" when you develop page and "false" if you goes public
